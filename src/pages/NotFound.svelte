66801cdaae95bc5c59bb76f86bc79e4ba41a4842
mrcocoball <gunmaru93@gmail.com>
2023-04-16 17:58:28 +0900

    #9 - NotFound용 화면 컴포넌트 추가

---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,9 @@
+<script>
+  import '../styles/auth.css'
+</script>
+
+<main class="form-signin w-100 m-auto">
+  <h1 class="h3 mb-3 fw-normal">잘못된 접근입니다.</h1>
+  <br>
+  <h6>잘못된 경로로 접근하였습니다.</h6>
+</main>
