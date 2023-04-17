4ca684004b854b911009a309439f65cb3a2573b6
mrcocoball <gunmaru93@gmail.com>
2023-04-17 20:18:45 +0900

    #9 - 장소 관련 컴포넌트 제작
    * 장소 페이지 컴포넌트
    * 장소 검색 결과 리스트 컴포넌트
    * 장소 검색 결과 지도 컴포넌트
    * 장소 검색 결과 썸네일 컴포넌트
    * 카카오맵 API 관련 스크립트 추가

---
+++ src/components/places/PlaceList.svelte
@@ -0,0 +1,96 @@
+<script>
+  import { places, placeDetail, requestPath, currentPlacesPage, currentPlacePaginationBar } from '../../store/places/placeStore.js'
+  import PlaceMap from "../../components/places/PlaceMap.svelte";
+  import PlaceThumb from './PlaceThumb.svelte';
+  import PlaceDetail from './PlaceDetail.svelte';
+
+  let detailMode = false
+
+  let categories = [
+    { 'id': 'AT4', 'name': '관광명소' },
+    { 'id': 'CE7', 'name': '카페' },
+    { 'id': 'CT1', 'name': '문화시설' },
+    { 'id': 'FD6', 'name': '음식점' },
+    { 'id': 'SW8', 'name': '지하철' },
+  ]
+
+  let initValues = {
+    formAddress: '',
+    formCategories: []
+  }
+
+  const searchPlaces = async () => {
+    try {
+      await places.fetchPlaces(initValues.formAddress, initValues.formCategories)
+    }
+    catch(error) {
+      alert(error.response.data.msg)
+    }
+  }
+
+  const setPage = async (pageNum) => {
+    try {
+      await places.fetchPlacesByPage($requestPath, pageNum)
+      //currentPageNum = pageNum
+      //currentPlacesPage.setPage(pageNum)
+    }
+    catch(error) {
+      alert(error.response.data.msg)
+    }
+  }
+
+  const onDetailMode = (place_id) => {
+    detailMode = true
+    goPlaceDetail(place_id)
+  }
+
+  const goPlaceDetail = async (place_id) => {
+    try {
+      await placeDetail.getPlace(place_id.toString())
+    }
+    catch(error) {
+      alert(error.response.data.msg)
+    }
+  }
+
+</script>
+
+{#if detailMode}
+<div class="place_detail">
+  {#if $placeDetail}
+    <PlaceDetail {placeDetail} bind:detailMode={detailMode} />
+  {/if}
+</div>
+{:else}
+<div class="place_search_form">
+  <input type="text" name="address" autocomplete="off" bind:value={initValues.formAddress} />
+  {#each categories as category}
+    <input type="checkbox" value={category.id} bind:group={initValues.formCategories} /> {category.name}
+  {/each}
+  <button class="btn btn-search" on:click={searchPlaces}>검색하기</button>
+</div>
+
+<div class="place_list">
+  <ul>
+    {#each $places.data.content as place, index}
+      <li on:click={() => onDetailMode(place.place_id)}>
+        <PlaceThumb {place} {index} />
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<div class="place_pagination">
+  <ul>
+    {#each $currentPlacePaginationBar as pageButton}
+      <li>
+        <a class={pageButton === $currentPlacesPage ? "btn-page-active" : "btn-page"} href="" on:click={() => setPage(pageButton)}>{pageButton+1}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<div class="place_map">
+  <PlaceMap {places} />
+</div>
+{/if}
--- src/pages/places/Places.svelte
+++ src/pages/places/Places.svelte
@@ -1,12 +1,10 @@
 <script>
   import AuthHeader from "../../components/auth/AuthHeader.svelte";
   import PlaceList from "../../components/places/PlaceList.svelte";
-  import PlaceMap from "../../components/places/PlaceMap.svelte";
 
 </script>
 
 <AuthHeader />
 <main class="place-main">
   <PlaceList />
-  <PlaceMap />
 </main>
