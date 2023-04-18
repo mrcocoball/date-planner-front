ee474c39450cb87a41ead51aa659f323a5f5dcb0
mrcocoball <gunmaru93@gmail.com>
2023-04-18 20:41:05 +0900

    #16 - GitHub Pages를 위한 설정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/date-planner-front/',
   plugins: [svelte()],
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  },
 })
