6089e461fc2eda67643554d906ed4e96da4389d2
mrcocoball <gunmaru93@gmail.com>
2023-04-18 21:40:40 +0900

    #16 - 수정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/date-planner-front/',
   plugins: [svelte()],
   build: {
     target: 'esnext' //browsers can handle the latest ES features
