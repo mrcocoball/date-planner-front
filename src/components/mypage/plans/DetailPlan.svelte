0fbd2ce61e60df8cde4947257091a45eb97b34e0
mrcocoball <gunmaru93@gmail.com>
2023-04-20 23:18:57 +0900

    #22 - 플랜 상세 화면, 세부 플랜, 플랜 지도 컴포넌트 추가

---
+++ src/components/mypage/plans/DetailPlan.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let detailPlan
+  export let index
+</script>
+
+<div class="detailPlan_thumb">
+  <span>{index+1}</span>
+  <span>{detailPlan.placeName}</span>
+  <span>{detailPlan.addressName}</span>
+</div>
