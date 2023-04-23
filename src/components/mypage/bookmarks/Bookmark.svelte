d0ff000d5d6c14af9a12a4d15a0031453baecb62
mrcocoball <gunmaru93@gmail.com>
2023-04-23 13:24:19 +0900

    #27 - 북마크 목록 및 썸네일 컴포넌트 추가
    북마크 장소 세부 정보에 대한 처리는 장소 세부 정보 컴포넌트 활용

---
+++ src/components/mypage/bookmarks/Bookmark.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let bookmark
+  export let index
+</script>
+
+<div class="bookmark_thumb">
+  <span>{index+1}</span>
+  <span>{bookmark.placeName}</span>
+  <span>{bookmark.addressName}</span>
+  <span>{bookmark.createdAt}</span>
+</div>
---
+++ src/pages/mypage/bookmarks/Bookmarks.svelte
@@ -0,0 +1,12 @@
+<script>
+  import AuthHeader from "../../../components/auth/AuthHeader.svelte";
+  import BookmarkList from "../../../components/mypage/bookmarks/BookmarkList.svelte";
+
+  let bookmarkSearchMode = false
+  
+</script>
+
+<AuthHeader />
+<main class="bookmark-main">
+  <BookmarkList {bookmarkSearchMode} />
+</main>
