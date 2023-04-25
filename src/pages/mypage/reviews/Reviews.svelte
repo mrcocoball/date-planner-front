612d1e85404e6a8d714e93906a7b5e1829624ff2
mrcocoball <gunmaru93@gmail.com>
2023-04-25 22:48:11 +0900

    #51 - 리뷰 조회 컴포넌트 디자인 수정

--- src/pages/mypage/reviews/Reviews.svelte
+++ src/pages/mypage/reviews/Reviews.svelte
@@ -1,9 +1,11 @@
 <script>
+  import Footer from "../../../components/Footer.svelte";
   import AuthHeader from "../../../components/auth/AuthHeader.svelte";
   import ReviewList from "../../../components/mypage/reviews/ReviewList.svelte";
 </script>
 
 <AuthHeader />
-<main class="review-main">
+<main class="container-fluid review-main">
   <ReviewList />
+  <Footer />
 </main>
