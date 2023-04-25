2366e99bf4e98592c362aafb74488eba3e7815c0
mrcocoball <gunmaru93@gmail.com>
2023-04-25 11:23:59 +0900

    #45 - 장소 검색, 장소 상세 정보 컴포넌트 디자인 수정

--- src/components/places/PlaceList.svelte
+++ src/components/places/PlaceList.svelte
@@ -10,14 +10,6 @@
   let searchMode = false
   let bookmarkSearchMode = false
 
-  let categories = [
-    { 'id': 'AT4', 'name': '관광명소' },
-    { 'id': 'CE7', 'name': '카페' },
-    { 'id': 'CT1', 'name': '문화시설' },
-    { 'id': 'FD6', 'name': '음식점' },
-    { 'id': 'SW8', 'name': '지하철' },
-  ]
-
   let initValues = {
     formAddress: '',
     formCategories: []
@@ -36,6 +28,8 @@
   const setPage = async (pageNum) => {
     try {
       await places.fetchPlacesByPage($requestPath, pageNum)
+      let element = document.getElementById("main_nav")
+      element.scrollIntoView();
     }
     catch(error) {
       alert(error.response.data.msg)
@@ -43,6 +37,8 @@
   }
 
   const onDetailMode = (place_id) => {
+    let element = document.getElementById("main_nav")
+    element.scrollIntoView();
     detailMode = true
     goPlaceDetail(place_id)
   }
@@ -83,46 +79,64 @@
 </script>
 
 {#if detailMode}
-<div class="place_detail">
+<div class="place-detail">
   {#if $placeDetail}
     <PlaceDetail {placeDetail} {bookmarkSearchMode} bind:detailMode={detailMode} on:detail-off={offDetailMode} />
   {/if}
 </div>
 {:else}
-<div class="place_search_form">
-  <input type="text" name="address" autocomplete="off" bind:value={initValues.formAddress} />
-  {#each categories as category}
-    <input type="checkbox" value={category.id} bind:group={initValues.formCategories} /> {category.name}
-  {/each}
-  <button class="btn btn-search" on:click={searchPlaces}>검색하기</button>
+<div class="place-search-form">
+  <div class="place-search-form-header">
+    <h4>장소 검색하기</h4>
+    <span>카테고리 태그와 주소를 입력하여 검색해보세요!</span>
+  </div>
+  <input type="checkbox" value="AT4" id="cb1" bind:group={initValues.formCategories} />
+  <label for="cb1">관광명소</label>
+  <input type="checkbox" value="CE7" id="cb2" bind:group={initValues.formCategories} />
+  <label for="cb2">카페</label>
+  <input type="checkbox" value="CT1" id="cb3" bind:group={initValues.formCategories} />
+  <label for="cb3">문화시설</label>
+  <input type="checkbox" value="FD6" id="cb4" bind:group={initValues.formCategories} />
+  <label for="cb4">음식점</label>
+  <input type="checkbox" value="SW8" id="cb5" bind:group={initValues.formCategories} />
+  <label for="cb5">지하철</label>
+  <div class="place-search-bar">
+    <input type="text" class="form-control address-input" name="address" placeholder="주소를 입력해주세요 (예 : 서울시 관악구 / 서초동)" autocomplete="off" bind:value={initValues.formAddress} />
+    <button class="btn btn-search" on:click={searchPlaces}>검색</button>
+  </div>
 </div>
 
-{#if searchMode}
-<div class="place_list">
-  <ul>
-    {#each $places.data.content as place, index}
-      <li on:click={() => onDetailMode(place.place_id)}>
-        <Place {place} {index} />
-      </li>
-      {#if placeSearchMode}
-      <button class="btn btn-search" on:click={() => sendAddress(place.place_id, place.place_name)}>목적지 선택</button>
-      {/if}
-    {/each}
-  </ul>
+<div class={searchMode ? "place-information" : "place-information-empty"}>
+  {#if searchMode}  
+  <div class="place-list">
+    <div>
+      <h4>검색 결과</h4>
+    </div>
+    <div>
+      <PlaceMap {places} />
+      {#each $places.data.content as place, index}
+      <ul class="list-group">
+        <li class="list-group-item place-thumb-item" on:click={() => onDetailMode(place.place_id)}>
+          <Place {place} {index} />
+        </li>
+        {#if placeSearchMode}
+        <button class="btn btn-search" on:click={() => sendAddress(place.place_id, place.place_name)}>목적지 선택</button>
+        {/if}
+      </ul>  
+      {/each}
+      <nav id="pagination" aria-label="Page navigation">
+        <div class="place-pagination">
+          <ul class="pagination justify-content-center">
+            {#each $currentPlacePaginationBar as pageButton}
+              <li class={pageButton === $currentPlacesPage ? "page-item active" : "page-item"} aria-current="page">
+                <a class="page-link" href="" on:click={() => setPage(pageButton)}>{pageButton+1}</a>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+  {/if}
 </div>
-
-<div class="place_pagination">
-  <ul>
-    {#each $currentPlacePaginationBar as pageButton}
-      <li>
-        <a class={pageButton === $currentPlacesPage ? "btn-page-active" : "btn-page"} href="" on:click={() => setPage(pageButton)}>{pageButton+1}</a>
-      </li>
-    {/each}
-  </ul>
-</div>
-
-<div class="place_map">
-  <PlaceMap {places} />
-</div>
-{/if}
 {/if}
