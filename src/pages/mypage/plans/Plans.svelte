82b46d19f18a81d474ba651a13227e776714c963
mrcocoball <gunmaru93@gmail.com>
2023-04-25 17:26:55 +0900

    #47 - 플랜 조회, 플랜 상세 정보 컴포넌트 디자인 수정

--- src/pages/mypage/plans/Plans.svelte
+++ src/pages/mypage/plans/Plans.svelte
@@ -1,10 +1,12 @@
 <script>
   import AuthHeader from "../../../components/auth/AuthHeader.svelte";
   import PlanList from "../../../components/mypage/plans/PlanList.svelte";
+  import Footer from "../../../components/Footer.svelte";
 
 </script>
 
 <AuthHeader />
-<main class="plan-main">
+<main class="container-fluid plan-main">
   <PlanList />
+  <Footer />
 </main>
