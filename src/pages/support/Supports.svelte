b13f9139f4b9e78af4dc3786851471058e2e4e52
mrcocoball <gunmaru93@gmail.com>
2023-04-26 10:16:38 +0900

    #55 - 고객센터 인덱스 페이지 컴포넌트 디자인 수정

--- src/pages/support/Supports.svelte
+++ src/pages/support/Supports.svelte
@@ -1,4 +1,5 @@
 <script>
+  import Footer from "../../components/Footer.svelte";
   import AuthHeader from "../../components/auth/AuthHeader.svelte";
   import { router } from 'tinro'
 
@@ -11,28 +12,38 @@
 </script>
 
 <AuthHeader />
-<main class="supports_main">
-  <div class="supports">
+<main class="container-fluid supports-main">
+  <div class="supports-header">
     <h2>고객센터</h2>
-    <p>도움이 필요하신가요? 성심껏 도와드리겠습니다.</p>
+    <span>도움이 필요하신가요? 성심껏 도와드리겠습니다.</span>
+    <br>
+    <span>고객센터 운영 시간 : 평일 오전 9시~오전 12시, 오후 1시~6시</span>
   </div>
 
-  <div class="announcements">
-    <h4>공지사항</h4>
-    <p>새로운 소식, 이벤트, 업데이트와 같은 공지사항을 모아두었습니다.</p>
-    <button class="btn btn-search" on:click={goAnnouncements}>확인하기</button>
+  <div class="p-5 mb-4 bg-body-tertiary border rounded-3">
+    <div class="container-fluid py-3 announcement">
+      <h1>공지사항</h1>
+      <p>새로운 소식, 이벤트, 업데이트와 같은 공지사항을 모아두었습니다.</p>
+      <button class="btn btn-search btn-lg" on:click={goAnnouncements}>확인하기</button>
+    </div>
   </div>
 
-  <div class="faqs">
-    <h4>FAQ</h4>
-    <p>여러분이 자주 질문해주시는 내용들을 모아두었습니다.</p>
-    <button class="btn btn-search" on:click={goFaqs}>확인하기</button>
+  <div class="row align-items-md-stretch" style="margin-bottom: 39px;">
+    <div class="col-md-6">
+      <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+        <h2>자주 묻는 질문</h2>
+        <p>여러분이 자주 질문해주시는 내용들을 모아두었습니다.</p>
+        <button class="btn btn-search" on:click={goFaqs}>확인하기</button>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+        <h2>질문 / 답변</h2>
+        <p>도움이 필요하신 내용에 대해 질문해주시면 도움을 드리겠습니다.</p>
+        <button class="btn btn-search" on:click={goQnas}>확인하기</button>
+      </div>
+    </div>
   </div>
 
-  <div class="qnas">
-    <h4>Q&A</h4>
-    <p>도움이 필요하신 내용에 대해 질문해주시면 도움을 드리겠습니다. (로그인 필요)</p>
-    <button class="btn btn-search" on:click={goQnas}>확인하기</button>
-  </div>
-
+  <Footer />
 </main>
