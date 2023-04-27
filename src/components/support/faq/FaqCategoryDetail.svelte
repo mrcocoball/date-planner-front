aab0b18ec74c1ad6f0a647e4bf42fc56975835df
mrcocoball <gunmaru93@gmail.com>
2023-04-27 09:09:17 +0900

    #59 - FAQ 상세 화면 컴포넌트 디자인 수정

--- src/components/support/faq/FaqCategoryDetail.svelte
+++ src/components/support/faq/FaqCategoryDetail.svelte
@@ -1,9 +1,32 @@
 <script>
-  import { createEventDispatcher, onDestroy } from 'svelte'
+  import { afterUpdate, createEventDispatcher, onDestroy } from 'svelte'
   import { faqDetail } from '../../../store/faqs/faqStore'
   import FaqAnswer from './FaqAnswer.svelte';
   export let faqDetailId
   export let detailMode
+
+  let categoryText = ''
+
+  afterUpdate(() => {
+    if ($faqDetail.data.id == '1') {
+      categoryText = '장소 정보와 관련된 질문과 답변입니다.'
+    }
+    else if ($faqDetail.data.id == '2') {
+      categoryText = '계정 관련 질문과 답변입니다.'
+    }
+    else if ($faqDetail.data.id == '3') {
+      categoryText = '이벤트 관련 질문과 답변입니다.'
+    }
+    else if ($faqDetail.data.id == '4') {
+      categoryText = '서비스 이용 중 발생한 오류에 대한 질문과 답변입니다.'
+    }
+    else if ($faqDetail.data.id == '5') {
+      categoryText = '기타 문의입니다.'
+    }
+    else {
+      categoryText = ''
+    }
+  })
 
   const dispatch = createEventDispatcher()
 
@@ -19,16 +42,23 @@
 
 </script>
 
-<div>
-  <button class="btn btn-cancel" on:click={offDetailMode}>창 숨기기</button>
+<div class="detail-top-button">
+  <button class="btn-close" on:click={offDetailMode} aria-label="Close"></button>
 </div>
 
-<div class="faq_answer">
-  <h4>{$faqDetail.data.categoryName}</h4>
+<div class="faq-answer-header">
+  <h2>{$faqDetail.data.categoryName}</h2>
+  <span>{categoryText}</span>
+  <br>
+  <span>'펼치기'를 통해 질문에 대한 답변을 확인할 수 있습니다.</span>
 </div>
 
-<div class="faq_answer_list">
+<div class="faq-answer-list">
 {#each $faqDetail.data.favoriteAnswers as favoriteAnswer, index}
-  <FaqAnswer {favoriteAnswer} {index} />
+  <ul class="list-group">
+    <li class="list-group-item faq-answer-thumb-item">
+      <FaqAnswer {favoriteAnswer} {index} />
+    </li>
+  </ul>
 {/each}
 </div>
