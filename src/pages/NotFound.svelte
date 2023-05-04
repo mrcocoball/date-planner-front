1056185775a54bb6eb841ef317f8e00e35ef1969
mrcocoball <gunmaru93@gmail.com>
2023-05-05 00:38:53 +0900

    #93 - 에러 페이지 수정

--- src/pages/NotFound.svelte
+++ src/pages/NotFound.svelte
@@ -1,10 +1,17 @@
 <script>
+  import { router } from 'tinro'
 
+  const goMain = () => {
+    router.goto('/')
+    let element = document.getElementById("main_nav")
+    element.scrollIntoView();
+  }
 </script>
 
-<main class="container-fluid not-found-box">
-  <h1 class="h3 mb-3 fw-normal">잘못된 접근입니다.</h1>
-  <br>
-  <h6>잘못된 경로로 접근하였습니다.</h6>
-  <br>
+<main class="container-fluid auth-main">
+  <div class="text-center">
+    <img src="/images/error-background.png" style="margin-bottom: 20px;">
+    <h1 class="h3 mb-3 fw-normal">오류가 발생하였습니다.</h1>
+    <button class="w-100 btn btn-cancel" style="margin-top: 20px;" on:click={goMain}>처음 화면으로 이동</button>
+  </div>
 </main>
